f17fa8a290500c93dbae1c6c19594e11d3c82a34
Ricardo <ricardofps66@gmail.com>
2022-05-14 13:47:52 -0300

    Updating constructor

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
 import organizeFile
-host = "192.168.3.20"
-username = "admin"
-password = ""
-port = 22
-command = "export"
 
-file = open("/home/ricardo/Documentos/Backup/mikrotik.txt","w")
-
-ssh = organizeFile.paramiko.SSHClient()
-
-ssh.set_missing_host_key_policy(organizeFile.paramiko.AutoAddPolicy())
-
-ssh.connect(host, port, username, password)
-
-stdin, stdout, stderr = ssh.exec_command(command)
-
-lines = stdout.readlines()
-doc = ' '.join(lines)
-file.write(doc)
-file.close()
-print(lines)
-
+if __name__ == "__main__":
+    organizeFile.exec()
 
